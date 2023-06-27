0583aff109b568cb50d04435a8c4f4fe5ee40bf3
katarsis <andrianow817@gmail.com>
2023-06-27 22:56:34 +0700

    fixing navbar bug

--- js/navbarHandler.js
+++ js/navbarHandler.js
@@ -1,32 +1,34 @@
+let btn_status = false
+const navbar = document.querySelector(".navbar-container")
+const navbar_btn = document.querySelector(".navbar-btn")
 function navbarHandler() {
-    const navbar = document.querySelector(".navbar-container")
-    const navbar_btn = document.querySelector(".navbar-btn")
-    let btn_status = false
-    if (window.innerWidth <= 768) {
 
-        window.addEventListener("resize", ev => {
-            if (window.innerWidth <= 768) {
-                btn_status = false
-                navbar.style.display = "none"
-            } else {
-                navbar.style.display = "flex"
 
-            }
-        })
+    window.addEventListener("resize", ev => {
+        if (window.innerWidth <= 768) {
+            btn_status = false
+            navbar.style.display = "none"
+        } else {
+            navbar.style.display = "flex"
 
-        navbar_btn.addEventListener("click", ev => {
-            btn_status = !btn_status
-            console.log(btn_status);
-            if (btn_status) {
-                navbar.style.display = "flex"
-            } else {
+        }
+    })
 
-                navbar.style.display = "none"
-            }
-        })
+    navbar_btn.addEventListener("click", navbarButtonHandler)
 
-    }
 
 }
+
+function navbarButtonHandler(ev) {
+    btn_status = !btn_status
+    console.log(btn_status);
+    if (btn_status) {
+        navbar.style.display = "flex"
+    } else {
+
+        navbar.style.display = "none"
+    }
+}
+
 
 export default navbarHandler
